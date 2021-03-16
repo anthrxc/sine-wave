9e0e3b9b0214d2f873c8baf0fc25c88e0ebbb01d
aanthr0 <anthromadayt@gmail.com>
2021-03-16 14:45:04 +0100

    Restarting the project

--- src/index.js
+++ src/index.js
@@ -10,26 +10,5 @@ client.on('ready', () => {
     console.log('---------------------------------------------\nSine Wave is ready to bleed people\'s ears out\n---------------------------------------------');
 });
 
-client.on('guildCreate', async guild => {
-
-});
-
-client.on('message', async message => {
-    console.log(message.guild.me.voice.channel);
-    if(message.mentions.members.find(user => user.id == "820626962434621450")) {
-        if(message.member.voice.channel) {
-            if(message.guild.me.voice.channnel != message.member.voice.channel) return;
-            if(message.member.voice.channel.viewable && message.member.voice.channel.speakable) {
-                message.member.voice.channel.join().then(bot => {
-                    bot.play(path.join(__dirname, "sine-wave.mp3"));
-                });
-                setTimeout(() => message.member.voice.channel.leave(), 35000);
-            }
-            else return;
-        }
-        else return;
-    }
-    else return;
-});
 
 client.login(process.env.TOKEN);
