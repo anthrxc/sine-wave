d94cf819044ebe4886de0adac074e230aeac9aee
aanthr0 <anthromadayt@gmail.com>
2021-03-16 18:34:15 +0100

    Project finished

--- src/index.js
+++ src/index.js
@@ -10,5 +10,34 @@ client.on('ready', () => {
     console.log('---------------------------------------------\nSine Wave is ready to bleed people\'s ears out\n---------------------------------------------');
 });
 
+client.on('voiceStateUpdate', async voice => {
+    if(!voice.channel) {
+        if(voice.id == client.user.id) {
+            if(voice.deaf) return;
+            else voice.setDeaf(true, "Self-deaf");
+        }
+        else return;
+    }
+    else return;
+});
+
+client.on('message', async message => {
+    if(message.mentions.members.find(x => x.id == client.user.id)) {
+        if(message.guild.me.voice.channel || !message.member.voice.channel) return;
+        else {
+            if(message.member.voice.channel.viewable && message.member.voice.channel.joinable) {
+                const channel = message.member.voice.channel;
+                channel.join().then(
+                    voice => {
+                        voice.play(path.join(__dirname, "sine-wave.mp3"));
+                    }
+                );
+                setTimeout(() => { channel.leave(); }, 32000);
+            }
+            else return;
+        }
+    }
+    else return;
+});
 
 client.login(process.env.TOKEN);
